cdd0cdc13ed00250a8279c2dea1e0c533d37a50e
erikbernard <erikbrsilva@gmail.com>
2023-08-13 14:48:53 -0300

    feat: craeted model contact and add admin

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import  timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    emai = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    descrition = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
