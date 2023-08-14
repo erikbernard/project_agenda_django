4edaa44f73d300ada4230765271e55da81d17e19
erikbernard <erikbrsilva@gmail.com>
2023-08-13 23:54:50 -0300

    feat: add imageField in model and add setting

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,8 @@ class Contact(models.Model):
     emai = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     descrition = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
